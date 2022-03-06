d3815fc183e1d24ef78b83d0e095bb76ff32a434
GauravDeshmukh17 <gauravdeshmukh1703@gmail.com>
2022-03-06 21:32:28 +0530

    JavaScript started

---
+++ index.js
@@ -0,0 +1,44 @@
+/*var a;
+
+a=12;
+
+console.log(a);
+console.log(typeof a);
+
+a="Gaurav"
+
+console.log(a)
+console.log(typeof a)
+
+var str="My Name is Gaurav \nDeshmukh";
+console.log(str);
+
+str=`hello`;
+console.log(str);
+
+var str1=`India is my country
+and all indians are my brothers and sisters`
+console.log(str1);
+
+var r=21;
+var r=32;  // Redeclaration of variable is allowed
+console.log(r);
+
+// To solve this problem we use 'let' keyword
+
+let m=45;
+// let m=56; Not allowed
+
+var num=200;
+console.log(`Half of ${num} is ${num/2}`);
+
+let a1=5;
+let a2=9;
+
+console.log(`Sum = ${a1+a2}`);*/
+
+console.log("Table of 8");
+for(var i=1;i<=10;i++){
+
+    console.log(i*8);
+}
