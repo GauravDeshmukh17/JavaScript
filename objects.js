36814cd2cf796579fd39d933b11e92fd4357c977
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2022-04-18 16:19:12 +0530

    objects.js file created

---
+++ objects.js
@@ -0,0 +1,18 @@
+let student={
+
+    name:"Gaurav",
+    age:21,
+    Gender:"Male",
+    College:"ICOER_Wagholi",
+    Address :["At : Dapegaon","Post:Jawalaga(P.D)","Tq : Ausa","Dist : Latur",413520],
+    Details:{
+
+        Primary_Education:"Emmanuel Public School , Dapegaon",
+        Secondary_Education:"Dayanand Science College , Latur",
+        Degree:"Pursuing from-JSPM'S IOCER , Wagholi , Pune",  
+    }
+ 
+}
+
+console.log(student);
+
