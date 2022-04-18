ebf3f51e02564b8cfb64410a82defee23042446b
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2022-04-18 15:24:23 +0530

    string.js modified

--- strings.js
+++ strings.js
@@ -39,3 +39,22 @@ var str4="  Hello    Every     one     ";
 console.log(str4["length"]);
 var str4=str4.trim();
 console.log(str4.length);
+
+function getdata(){
+
+   let b=100; 
+   return b;
+}
+
+console.log(""+getdata);
+console.log(getdata());
+
+let a=function sum(c,d){
+
+    return c+d;
+}
+
+console.log(""+a);
+console.log("Sum = "+a(12,45));
+
+
