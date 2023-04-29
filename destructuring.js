582bb702ab35d0e5793fd7dde84198420b496410
gauavdeshmukh1703@gmail.com <gauravdeshmukh1703@gmail.com>
2023-04-29 22:18:27 +0530

    :art: destructuring of array and object

---
+++ destructuring.js
@@ -0,0 +1,20 @@
+// array destructuring
+// [a,b]=["Gaurav",10];
+// console.log(a);
+// console.log(b);
+
+// [n1, ,n3]=[1,2,3];
+// console.log(n1);
+// console.log(n3);
+
+
+
+// object destructuring
+let obj={
+    name:"Gaurav",
+    age:22
+}
+
+let {age,name}=obj;
+console.log(age);
+console.log(name);
